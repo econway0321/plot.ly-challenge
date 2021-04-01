c6edb3b29186d46df8ea684757bbb034e7a24078
econway0321 <econway0321@gmail.com>
2021-03-31 20:55:50 -0600

    bubble

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,14 +18,14 @@ d3.json("samples.json").then((samples)=> {
 	console.log(sampleid)
 	var otuid = result.otu_ids;
 	var samid = result.sample_values;
-	var id = samples.samples[0].otu_ids
-	var values = samples.samples[0].sample_values
+	// var id = samples.samples[0].otu_ids;
+	// var values = samples.sample_values;
+
 	
 	console.log(samples.samples)
 	console.log(otuid)
 	console.log(samid)
-	console.log(id)
-	console.log(values)
+	
 
 	var y_tick = otuid.map(x=>`otu ${x}`).slice(0,10).reverse()
 
@@ -40,7 +40,7 @@ d3.json("samples.json").then((samples)=> {
 	var bar_data = [trace1];
 
 	var bar_layout = {
-		title: 'Top 10 Bacteria Culters Found',
+		title: 'Top 10 Bacteria',
 		margin: {
 			t:30,
 			l:150
@@ -50,10 +50,12 @@ d3.json("samples.json").then((samples)=> {
 	Plotly.newPlot('bar', bar_data,bar_layout);
 
 	var trace2 = {
-		x: id,
-		y: values,
+		type: bubble,
+		x: otuid,
+		y: samid,
 		mode: 'markers',
 		marker: {
+			
 			size: [40,60,80,100]
 		}
 	};
@@ -61,7 +63,7 @@ d3.json("samples.json").then((samples)=> {
 	var bubble_data = [trace2];
 
 	var bubble_layout = {
-		title: 'Test',
+		title: 'Sample Values',
 		height: 600,
 		width: 600
 	}
@@ -81,24 +83,6 @@ function optionChanged(newsample){
 
 
 
-// var trace2 = {
-// 		x: id,
-// 		y: values,
-// 		mode: 'markers',
-// 		marker: {
-// 			size: [40,60,80,100]
-// 		}
-// 	};
-
-// 	var bubble_data = [trace2]
-
-// 	var layout = {
-// 		title: 'Test',
-// 		height: 600,
-// 		width: 600
-// 	}
-
-// 	Plotly.newPlot('bubble',bubble_data,layout);
 
 
 
