4c7729d34485bb3b8836cb2bfe092918370da1db
econway0321 <econway0321@gmail.com>
2021-03-31 20:37:37 -0600

    bubble

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,21 +13,25 @@ updatePage();
 
 function buildPlot(sampleid){
 d3.json("samples.json").then((samples)=> {
-	var name = samples.names;
+	var name = samples.name;
 	var result = samples.samples.filter(x=>x.id==sampleid)[0]
 	console.log(sampleid)
 	var otuid = result.otu_ids;
 	var samid = result.sample_values;
+	var id = samples.samples[0].otu_ids
+	var values = samples.samples[0].sample_values
 	
 	console.log(samples.samples)
 	console.log(otuid)
 	console.log(samid)
+	console.log(id)
+	console.log(values)
 
-	var y_tick = otuid.map(x=>`otu ${x}`).slice(0,10)
+	var y_tick = otuid.map(x=>`otu ${x}`).slice(0,10).reverse()
 
 	var trace1 = {
 	  	type: "bar",
-	  	x: samid.slice(0,10),
+	  	x: samid.slice(0,10).reverse(),
 	  	y: y_tick,
 	  	orientation: 'h'
 	  	
@@ -45,6 +49,26 @@ d3.json("samples.json").then((samples)=> {
 
 	Plotly.newPlot('bar', bar_data,bar_layout);
 
+	var trace2 = {
+		x: id,
+		y: values,
+		mode: 'markers',
+		marker: {
+			size: [40,60,80,100]
+		}
+	};
+
+	var bubble_data = [trace2];
+
+	var bubble_layout = {
+		title: 'Test',
+		height: 600,
+		width: 600
+	}
+
+
+	Plotly.newPlot('bubble',bubble_data,bubble_layout);
+
 });
 }
 
@@ -54,3 +78,28 @@ function optionChanged(newsample){
 	buildPlot(newsample)
 
 }
+
+
+
+// var trace2 = {
+// 		x: id,
+// 		y: values,
+// 		mode: 'markers',
+// 		marker: {
+// 			size: [40,60,80,100]
+// 		}
+// 	};
+
+// 	var bubble_data = [trace2]
+
+// 	var layout = {
+// 		title: 'Test',
+// 		height: 600,
+// 		width: 600
+// 	}
+
+// 	Plotly.newPlot('bubble',bubble_data,layout);
+
+
+
+
